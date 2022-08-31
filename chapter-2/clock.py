090da20acc2b601f471322d44ec69f5e3b2fcb76
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-08-31 20:20:27 +0000

    Added chapter 2 exercises

---
+++ chapter-2/clock.py
@@ -0,0 +1,12 @@
+# Exercise 2.3
+print(3.14, int(3.14))
+print(3.9999, int(3.9999))       # This doesn't round to the closest int!
+print(3.0, int(3.0))
+print(-3.999, int(-3.999))        # Note that the result is closer to zero
+print(int("23bottles"))          # THIS DOES NOT WORK AS A INTEGER
+print(float("123.45"))           # TURNS CLASS INTO FLOAT    
+print(type(float("123.45")))
+
+print(str(17))                #TURNS THE CLASS INTO STRING
+print(str(123.45))
+print(type(str(123.45)))
---
+++ chapter-2/sent.py
@@ -0,0 +1,4 @@
+# Exercise 2.5
+a = 123
+pi = 23
+time_in_seconds = 120
