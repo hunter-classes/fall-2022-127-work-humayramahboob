1117a4e04c3c2e15aef60d7cc092a4b3a23dec82
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-09-01 13:37:57 +0000

    modified root1.py

--- code/turtles/root1.py
+++ code/turtles/root1.py
@@ -4,7 +4,7 @@ for i in range(4):
 for number in range(5,10):
   print(number)
 
-for name in range ['able','baker','charlie']:
+for name in ['able','baker','charlie']:
   print(name)
   print(name.upper())
 print("The loop is done.")
