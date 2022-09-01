fc79a75ca57e162d7e512d3c19961d2809295942
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-09-01 13:44:56 +0000

    modified root1.py

--- code/turtles/root1.py
+++ code/turtles/root1.py
@@ -1,9 +1,11 @@
 for i in range(4):
   print(i)
 
+print("----------------------------")
 for number in range(5,10):
   print(number)
 
+print("----------------------------")
 for name in ['able','baker','charlie']:
   print(name)
   print(name.upper())
