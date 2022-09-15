51144456e23609a446c4da7293e6663580739a9e
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-09-15 02:40:16 +0000

    homework

---
+++ HW-9-15/ch7-seven.py
@@ -0,0 +1,8 @@
+# Write a function called is_even(n) that takes an integer as an argument and returns 
+# True if the argument is an even number and False if it is odd.
+
+def is_even(n):
+    if n%2==0:
+        return True
+    else:
+        return False
