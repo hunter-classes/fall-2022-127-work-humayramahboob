4c1b435edf10e030000f30c477b585647e99f4d3
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-09-19 13:33:08 +0000

    made changes to piglatin

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -4,11 +4,11 @@ def bondify(name):
   return a string in the form "Last First Last
   """
   
-  # find the first name
+  # find the first name and capitalize it
   location = name.find(' ')
   first = name[:location].capitalize()
   
-  # find the last name
+  # find the last name and capitalize it
   last = name[location+1:].capitalize()
   # write the string that returns
   return last + ' ' + first +' ' + last
@@ -31,9 +31,12 @@ def piglatin(word):
  try to also handle upper case word
  """
   
-  if word.startswith('a' or 'e' or 'i' or 'o' or 'u'):
+  if word.startswith('a' or 'e' or 'i' or 'o' or 'u'):   #vowel version
     result = word + 'yay'
-    return result
-  else:
+    final = result.capitalize()
+    return final
+  else:                                                  #regular version
     result = word[1:] + word[0] + 'ay'
-    return result
+    final = result.capitalize()
+    return final
+  
