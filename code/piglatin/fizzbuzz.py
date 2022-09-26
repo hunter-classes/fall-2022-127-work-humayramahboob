82ac740b40ce9840a92e87df6a98ed9ea9c0e36a
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-09-26 20:34:46 +0000

    editted fizzbuz

--- code/piglatin/fizzbuzz.py
+++ code/piglatin/fizzbuzz.py
@@ -4,12 +4,12 @@
 
 i = 0
 while i<100:
-    i = i+1
-    if i%3==0:
-        print("fizz")
-    elif i%5==0:
-        print("buzz")
-    elif i%3==0 and i%5==0:
-        print("fizzbuzz")
-    else:
-        print(i)
+  i = i+1
+  if i%3==0 and i%5==0:
+    print("fizzbuzz")
+  elif i%3==0:
+    print("fizz")
+  elif i%5==0:
+    print("buzz")
+  else:
+    print(i)
