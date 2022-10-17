6de698d27a2fae0293211a3219c01d1dcfc7110c
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-10-17 13:05:53 +0000

    Added test runs for listlab

--- listlab.py
+++ listlab.py
@@ -5,7 +5,10 @@ def smalled_in_list(lst):
     if i < min:
       min = i
   return min
-  
+
+lst = [32,12,304,2,3,200,35,9]
+print(smalled_in_list(lst))
+
 # Write a function that returns a new list that contains all the odd items in the original list
 def odd_numbers(lst):
   odd_list = []
@@ -14,6 +17,8 @@ def odd_numbers(lst):
       odd_list.append(i)
   return odd_list
 
+print(odd_numbers(lst))
+
 # Write a function that takes a string and returns a new string where all the words are capitalized.
 def capitalized(s):
   words = s.split(" ")
@@ -21,6 +26,9 @@ def capitalized(s):
   for word in words:
     s1 = s1+" "+[word.capitalize()]
   return s1
+
+s = "hello my name is humayra and I go to hunter college"
+print(capitalized(s))
 
 # Write a function that takes a string and returns a new string with every word that's longer than 5 character turned into upper case
 def uppercase_long(s):
@@ -33,12 +41,16 @@ def uppercase_long(s):
       new_s= new_s+" "+[word]
   return new_s
 
+print(uppercase_long(s))
+
 # Write a function that takes a list of numbers and returns a new list with each item squared
 def squared(lst):
   squared_list= []
   for i in lst:
     squared_list.append(i*i)
   return squared_list
+
+print(squared(lst))
 
 # Write a function that takes two lists of numbers and returns a new list where each item is the corresponding values of the original lists added together. Ex [1,2,3] and [10,20,30] would return the list [11,22,33]
 def corresponding_sum(lst1,lst2):
@@ -49,6 +61,10 @@ def corresponding_sum(lst1,lst2):
     index = index + 1
   return total
 
+lst1 = [10,20,30]
+lst2 = [1,2,3]
+print(corresponding_sum(lst1,lst2))
+
 # chapter 10 # 10, 11, 12
 # 10. Count how many words in a list have length 5.
 def length_five(lst):
@@ -58,14 +74,20 @@ def length_five(lst):
       total = total + 1
   return total
 
+lst = ["hello","seven","sandwich","turkey","sentence","hi"]
+print(length_five(lst))
+
 # 11.Sum all the elements in a list up to but not including the first even number.
-def sum(lst):
+def sum_until_even(lst):
   sum = 0
   index = 0
   while lst[index]%2!=0:
     sum = sum + lst[index]
     index = index + 1
   return sum
+
+lst = [3,7,23,35,8,3,1]
+print(sum_until_even(lst))
 
 # Count how many words occur in a list up to and including the first occurrence of the word “sam”.
 def not_sam(lst):
@@ -77,3 +99,6 @@ def not_sam(lst):
   if lst[index]== "sam":
     sum = sum + 1
   return sum
+
+lst = ["test","running","hello","sandwich","sam","money","cash"]
+print(not_sam(lst))
