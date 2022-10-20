6e885fb9852e730b10ddea7e154a22e685bc4359
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-10-20 13:16:21 +0000

    added more list comprehensions

--- code/lists/advanced-lists.py
+++ code/lists/advanced-lists.py
@@ -30,11 +30,20 @@ for item in random_list:
 def square(x):
   return x*x
 
+# LIST COMPREHENSIONS
 counting = [x for x in range(5,10)]
 tens_v2 = [10 for x in range(15)]
 random_list_v2 = [random.randrange(5,10) for x in range(20)]
 random_list_v3 = [random.randrange(x) for x in range(1,100,5)]
 sentence = "now is the winter of our discontent"
 upper_case = [word.capitalize() for word in sentence.split()]
+shortwords = [word for word in sentence.split() if len(word) < 4]
+vwords = [word for word in sentence.split() if w[0] in 'aeiou']
 
 sqlist = [x*x for x in random_list_v3]
+
+l = [x for x in range(20)]
+# [x*x for x in l] <----------- returns squared
+# [x for x in l if x%2 == 1] <- returns odds
+# [x for x in l if x%2 == 0] <- returns even
+# [x for x in l if x > 10] <--- returns numbers > 10
