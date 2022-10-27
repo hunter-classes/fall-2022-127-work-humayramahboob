ad59817016d91b4b0d575e113a6b0149d9a20ec0
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-10-27 12:31:50 +0000

    Made madlibs one function

--- mad_libs/mad_libs.py
+++ mad_libs/mad_libs.py
@@ -1,17 +1,13 @@
 import random
+def mad_libs_filler(MAIN,NOUN,VERB,f):
 #EXTRA #1: I will make a function that opens a file and seperates the words on it line by line. def file_translator(f) 
-def file_translator(f):
   new_f=open(f)
   data = new_f.read()
-  lines =data.split('\n')
   words =data.split()
-  return words
-
-def mad_libs_filler(MAIN,NOUN,VERB,prompt):
   prompt_list=[]
   # EXTRA #2: (MAKING SURE THE MAIN CHARACTER STAYS THE SAME) To make sure you pick a main randomly and the same main is used throughout the madlibs solution, we need to use the random.choice() function outside the for function so it only picks randomly once.
   main= random.choice(MAIN)
-  for item in prompt:
+  for item in words:
 # We use .append(main) since we have a set person from the list to use all the time. For verbs and nouns, you pick random from the list multiple times.
     if item == "<MAIN>":
       prompt_list.append(main.capitalize())
@@ -39,10 +35,8 @@ VERB =["eat","sleep","dance","vape","jump","run","walk","trip"]
 MAIN = ["john","juan","mary","susan","bill","klee","tammy","drake"]
 
 #TESTING FIRST MAD LIBS STORY
-prompt = file_translator('one.dat')
-print(mad_libs_filler(MAIN,NOUN,VERB,prompt))
+print(mad_libs_filler(MAIN,NOUN,VERB,'one.dat'))
 
 print("----------------------------------------------------")
 #TESTING SECOND MAD LIBS STORY
-prompt = file_translator('two.dat')
-print(mad_libs_filler(MAIN,NOUN,VERB,prompt))
+print(mad_libs_filler(MAIN,NOUN,VERB,'two.dat'))
