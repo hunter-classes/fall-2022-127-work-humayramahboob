5e2812fb34ee17147732d730ee68119e12124d5a
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-10-27 12:26:49 +0000

    updated modes.py

--- mode/modes.py
+++ mode/modes.py
@@ -1,10 +1,10 @@
-# 1. findSmallest(l) which takes in a list of numbers and returns the value of the smallest number
-def findSmallest(l):
-  min = l[0]
+# 1. findLargest(l) which takes in a list of numbers and returns the value of the largest number
+def findLargest(l):
+  max = l[0]
   for i in l:
-    if i< min:
-      i = min
-  return min
+    if i> max:
+      i = max
+  return max
     
 # freq(l,v) which takes a list of numbers(l) and a value (v). The function will return the frequency of v, that is, the number of times v appears in l.
 def freq(l,v):
