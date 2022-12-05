782f60566a5024dbfeec43c6305ccbe96042f3f7
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-12-05 14:37:35 +0000

    added intro and basic cpp things

---
+++ c++/basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream
+
+int main()
+{
+  int a;
+  a = 10
+
+  if (a<20){
+    std::cout << "a <20\n";
+    std::cout << "alo in the true part\n";
+  }  
+  if (a< 20){
+    std::cout << "if part: a <20\n";
+  }
+    else {
+    std::cout << "else part: a >= 20\n";  
+  }
+}
--- c++/intro.cpp
+++ c++/intro.cpp
@@ -21,7 +21,23 @@
 
 int main()  // here to end of line is a comment 
 {
-  std::cout << "Hello World!" << std::endl;
+  int x=21;
+  double d = 21;
+  char c; 
+  bool tf = false;
+  std::string s= "this is a string";
+ 
+  std::cout << "Hello World!\n";
+  std::cout << "the var x = " << x << "\n";
   
+  std::cout << "Doubles: "<< d <<" "<< d/2 <<std::endl;
+  std::cout << "Ints:"<<x<< " "<< x/2<< std::endl;
+
+  c = 'x'; //a SINGLE character
+  std::cout<<c << "\n"
+
+  std::cout<<  "A true value:" << tf <<"\n"; //returns 1 if bool is true and 0 if bool is false
   return 0;
 }
+
+// use -wall when running to show the errors in the program
