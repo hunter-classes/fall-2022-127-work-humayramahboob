aadd00697c5b5b7e8faf3db0a2c5ada669eb079d
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-12-09 13:43:00 +0000

    edited data.py

--- data/data.py
+++ data/data.py
@@ -33,20 +33,21 @@ english = ""
 english = ", ".join(skilled_in_english)
 math = ""
 math = ", ".join(skilled_in_math)
-print("The states that have more skilled students in math:",math)
+print("The areas that have more skilled students in math:",math)
 print("------------------------------------------------------------")
-print("The states that have more skilled students in english:",english)
+print("The areas that have more skilled students in english:",english)
 print("------------------------------------------------------------")
-print("The states that have equally balanced students are:",balanced)
+print("The areas that have equally balanced students are:",balanced)
 print("------------------------------------------------------------")
 
 # ----------------------------------------------------- 
-#EXTRA 2 I COMPARED THE HIGH ENGLISH AVERAGED STATES TO THE WHITE ONLY PERCENTAGES OF THOSE STATES FROM TWO DIFFERENT CSV FILES.
+#EXTRA 2 I Compared the english and math averages of people with the highest white only percentage and lower white only percentge to see if they make a difference in averages.
 prompt = csv.reader(open("state_demographics.csv"))
 state_demographics = []
 for item in prompt:
   state_demographics.append(item)
 del state_demographics[0]
+
 max_white_only = 0
 index = 0
 for item in state_demographics:
@@ -54,7 +55,17 @@ for item in state_demographics:
     max_white_only = item[8]
     state = item[0]
     for line in school_scores:
-      if line[2] == "Maine":
+      if line[2] == state:
         math_grade = line[13]
         english_grade = line[9]
-print("In",state,"there are the most white only people with a percentage of",max_white_only,". Students have a english average of",english_grade,"and a math average of",math_grade,". States with more white people tend to have higher student averages.")
+
+min_white_only = item[8]
+for item in state_demographics:
+  if float(item[8]) < float(min_white_only):
+    min_white_only = item[8]
+    state1 = item[0] 
+    for line in school_scores:
+      if line[2] == state1:
+        math_grade1 = line[13]
+        english_grade1 = line[9]
+print("In",state,"there are the most white only people with a percentage of",max_white_only,". Students have a english average of",english_grade,"and a math average of",math_grade,". In",state1,"there are the least white only people with a percentage of",min_white_only,". Students have a english avaerage of",english_grade1,"and a math average of",math_grade1,".")
