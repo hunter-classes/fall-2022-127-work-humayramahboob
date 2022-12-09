2427dc20b88662e7259c7b21277f90baac6c9994
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-12-09 07:53:41 +0000

    finished data project

--- data/data.py
+++ data/data.py
@@ -1,6 +1,17 @@
+import csv
+
+reader = csv.reader(open("school_scores.csv"))
+school_scores = []
+for line in reader:
+  school_scores.append(line)
+del school_scores[0]
+
+max_test_takers = 0
+state = ""
+# I used the data from school scores to evaluate which state had the most amount of test takers.
 for line in school_scores:
-  if float(line[4]) > max_test_takers:
-    max_test_takers = float(line[4])
+  if int(line[4]) > max_test_takers:
+    max_test_takers = int(line[4])
     state = line[2]
 print("The highest amount of SAT test takers are",max_test_takers,"from",state,".")
 
@@ -10,9 +21,9 @@ skilled_in_math = []
 skilled_in_english = []
 equally_balanced = []
 for line in school_scores:
-  if float(line[9]) == float(line[12]):
+  if float(line[9]) == float(line[13]):
     equally_balanced.append(line[2])
-  elif float(line[9]) > float(line[12]):
+  elif float(line[9]) > float(line[13]):
     skilled_in_english.append(line[2])
   else:
     skilled_in_math.append(line[2])
@@ -37,8 +48,13 @@ for item in prompt:
   state_demographics.append(item)
 del state_demographics[0]
 max_white_only = 0
+index = 0
 for item in state_demographics:
   if float(item[8]) > float(max_white_only):
     max_white_only = item[8]
     state = item[0]
-print("In",state,"there are the most white only people with a percentage of",max_white_only)
+    for line in school_scores:
+      if line[2] == "Maine":
+        math_grade = line[13]
+        english_grade = line[9]
+print("In",state,"there are the most white only people with a percentage of",max_white_only,". Students have a english average of",english_grade,"and a math average of",math_grade,". States with more white people tend to have higher student averages.")
