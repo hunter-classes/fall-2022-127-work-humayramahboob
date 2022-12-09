f5c59edc08af9ade539d1da2971ddc101e2a47c3
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-12-09 07:41:35 +0000

    data project

--- data/data.py
+++ data/data.py
@@ -1,12 +1,6 @@
-import csv
-
-reader = csv.reader(open("school_scores.csv"))
-max_test_takers = 0
-state = ""
-# I used the data from school scores to evaluate which state had the most amount of test takers.
-for line in reader:
-  if int(line[4]) > max_test_takers:
-    max_test_takers = int(line[4])
+for line in school_scores:
+  if float(line[4]) > max_test_takers:
+    max_test_takers = float(line[4])
     state = line[2]
 print("The highest amount of SAT test takers are",max_test_takers,"from",state,".")
 
@@ -15,10 +9,10 @@ print("The highest amount of SAT test takers are",max_test_takers,"from",state,"
 skilled_in_math = []
 skilled_in_english = []
 equally_balanced = []
-for line in reader:
-  if int(line[9]) == int(line[12]):
+for line in school_scores:
+  if float(line[9]) == float(line[12]):
     equally_balanced.append(line[2])
-  elif int(line[9]) > int(line[12]):
+  elif float(line[9]) > float(line[12]):
     skilled_in_english.append(line[2])
   else:
     skilled_in_math.append(line[2])
@@ -28,23 +22,23 @@ english = ""
 english = ", ".join(skilled_in_english)
 math = ""
 math = ", ".join(skilled_in_math)
-print("The states that have more skilled students in math are",math,"and the states that have more skilled students in english are", english, ". While the states that have equally balanced students are",balanced)
+print("The states that have more skilled students in math:",math)
+print("------------------------------------------------------------")
+print("The states that have more skilled students in english:",english)
+print("------------------------------------------------------------")
+print("The states that have equally balanced students are:",balanced)
+print("------------------------------------------------------------")
 
 # ----------------------------------------------------- 
 #EXTRA 2 I COMPARED THE HIGH ENGLISH AVERAGED STATES TO THE WHITE ONLY PERCENTAGES OF THOSE STATES FROM TWO DIFFERENT CSV FILES.
-reader1 = csv.reader(open("state_demographics.csv"))
-high_english_averages = []
-for line in reader1:
-  if int(line[9]) == 4:
-    high_english_averages.append(line[2])
-
-white_only_percentage = []
-for line in reader:
-  for word in line:
-    if word in high_english_averages:
-      white_only_percentage.append(line[8])
-result = ""
-result = ", ".join(high_english_averages)
-result1= ""
-result1= ", ".join(white_only_percentage)
-print("The states",result,"have high english averages. The percentage of white only people in those states are",result1,". This concludes that states with the higher white only percentages have better english averages in school."
+prompt = csv.reader(open("state_demographics.csv"))
+state_demographics = []
+for item in prompt:
+  state_demographics.append(item)
+del state_demographics[0]
+max_white_only = 0
+for item in state_demographics:
+  if float(item[8]) > float(max_white_only):
+    max_white_only = item[8]
+    state = item[0]
+print("In",state,"there are the most white only people with a percentage of",max_white_only)
