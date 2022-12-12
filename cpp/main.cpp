40fd542f614e3496f5bed0d37031ba7fcc4bf635
humayramahboob <104864864+humayramahboob@users.noreply.github.com>
2022-12-12 14:30:10 +0000

    finished c++ project

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 // HERE IS A FUNCTION WHERE I ADD THE SUM OF THE SQUARES FOR THE RANGE A TO B. 
-//I used a for loop inside this function. 
-int sum_of_squares(int a, int b){
+//I used a for loop inside this function and a if loop within the for loop.
+int sum_of_even_squares(int a, int b){
   int c = 0;
   for (int i = a; i <= b; i++){
-    c= c + i*i;
+    if (i % 2 == 0){
+      c=c + i*i;
+    }
   }
   return c;
   }
@@ -12,8 +14,8 @@ int sum_of_squares(int a, int b){
 int main(){
   int a,b,i;
   a = 3;
-  b = 6;
-  int c= sum_of_squares(a,b);
+  b = 10;
+  int c= sum_of_even_squares(a,b);
   std::cout<<c<<'\n';
   return 0;
 }
